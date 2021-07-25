b3dc1f2352d5a700acb0085e45b72eb977b9884a
jamescalam <james.briggs@hotmail.com>
2021-07-25 08:43:47 +0100

    updated download script

--- sentence_embeddings_15K/download.py
+++ sentence_embeddings_15K/download.py
@@ -1,7 +1,13 @@
 import requests
+import os
 
 data_url = "https://raw.githubusercontent.com/jamescalam/data/main/sentence_embeddings_15K/"
 
+# create data directory to store data
+if not os.path.exists('./data'):
+    os.mkdir('./data')
+
+# download the numpy binary files (dense vectors)
 for i in range(57):
     if i < 10:
         i = '0' + str(i)
@@ -10,3 +16,9 @@ for i in range(57):
         for chunk in res:
             fp.write(chunk)
     print('.', end='')
+
+# and download the respective text file
+res = requests.get(f"{data_url}sentences.txt")
+with open(f"./data/sentences.txt", 'wb') as fp:
+    for chunk in res:
+        fp.write(chunk)
